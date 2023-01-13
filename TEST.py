6107e97c508e92663d00a4e7b276768defc0a8d2
TheJake289781 <105983747+TheJake289781@users.noreply.github.com>
2023-01-13 17:53:45 +0100

    Add files via upload

---
+++ TEST.py
@@ -0,0 +1,15 @@
+num1 = input("Enter The First Number ->")
+num2 = input("Enter The Sign ->")
+num3 = input("Enter The Second Number ->")
+
+if num2 == "*":
+    print(num1 * num3)
+
+if num2 == "+":
+    print(num1 + num3)
+
+if num2 == "-":
+    print(num1 - num3)
+
+if num2 == "/":
+    print(num1 / num3)
