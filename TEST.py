a7627fc1655e2eec74d73da4d7ec8b04867ed980
TheJake289781 <105983747+TheJake289781@users.noreply.github.com>
2023-01-20 19:18:28 +0100

    Update TEST.py

--- TEST.py
+++ TEST.py
@@ -1,5 +1,5 @@
 
-plus_or_times = input("+ or x or :? ")
+plus_or_times = input("+ or x or : or -? =  ")
 
 num1 = input("enter any number and press enter: ")
 num2 = input("enter another number and press enter: ")
@@ -7,6 +7,8 @@ num2 = input("enter another number and press enter: ")
 if plus_or_times == 'x': ans = float(num1) * float(num2)
 if plus_or_times == '+': anwser: int = float(num1) + float(num2)
 if plus_or_times == ':': answ: int = float(num1) // float(num2)
+if plus_or_times == '-': ans1: int = float(num1) - float(num2)
 if plus_or_times == ':': print(answ)
 if plus_or_times == '+': print(anwser)
 if plus_or_times == 'x': print(ans)
+if plus_or_times == '-': print(ans1)
